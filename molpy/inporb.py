f447cd0014c03f1d8ac610085700eccd46b81e87
steabert <steabert@users.noreply.github.com>
2015-11-26 13:31:47 +0100

    fallback when a supposed INPORB file fails to open/read

--- molpy/inporb.py
+++ molpy/inporb.py
@@ -14,9 +14,12 @@ class MolcasINPORB():
         initialize the INPORB file and prepare for reading or writing
         """
         if mode.startswith('r'):
-            with open(filename, 'r') as f:
-                firstline = next(f)
-            if not firstline.startswith('#INPORB'):
+            try:
+                with open(filename, 'r') as f:
+                    firstline = next(f)
+                if not firstline.startswith('#INPORB'):
+                    raise InvalidRequest
+            except:
                 raise InvalidRequest
 
         self.f = open(filename, mode)
