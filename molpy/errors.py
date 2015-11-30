70be6216c53d1e187b5fe11c7947b1c30152660d
steabert <steabert@users.noreply.github.com>
2015-11-30 14:47:40 +0100

    better error messages

--- molpy/errors.py
+++ molpy/errors.py
@@ -4,6 +4,9 @@ from . import export
 @export
 class Error(Exception):
     """ Base class for errors """
+    prefix = 'Error:'
+    def __str__(self):
+        return ' '.join((self.prefix, super().__str__()))
 
 
 @export
@@ -14,8 +17,10 @@ class UserError(Error):
 @export
 class InvalidRequest(Error):
     """ Wrong data was requested """
+    prefix = 'Error: invalid request:'
 
 
 @export
 class DataNotAvailable(Error):
     """ The data requested is not available """
+    prefix = 'Error: missing data:'
